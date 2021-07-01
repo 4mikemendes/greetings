efa9e4864dc62d966d912651c7bd0b6117dd71ac
이혜진 <4mikemendes@gmail.com>
2021-07-01 11:16:23 +0200

    greetings js

---
+++ script.js
@@ -0,0 +1,10 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "mike"
+let greeting = "welcome back "
+
+
+  welcomeEl.innerText = greeting + name
+
+  // welcomeEl.innerText =  welcomeEl.innerText + " 🐉"
+   welcomeEl.innerText += " 🐉"
